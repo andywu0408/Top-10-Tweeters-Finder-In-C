058d086908b19928f48de7380d6a17da085d691c
wizardmk <mska@ucdavis.edu>
2019-11-22 21:31:14 -0800

    Created MaxTweeter.c, did some basic input checking, for command line args

---
+++ maxTweeter.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define FILE_LINES_MAX = 20000
+#define LINE_CHARS_MAX = 1024
+
+int main (int argc, char* argv[]) {
+
+	// Checking for valid program input
+	// Must have 1 command line argument (filename)
+	if (argc != 2) {
+		printf("Invalid Input Format");
+		exit(1); // unsuccessful exit
+	}
+
+
+}
+
