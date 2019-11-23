325a36bf2c1fa8c331250ee235a72c84fe45fc89
wizardmk <mska@ucdavis.edu>
2019-11-22 22:06:38 -0800

    Checking if file exists

--- maxTweeter.c
+++ maxTweeter.c
@@ -6,13 +6,22 @@
 
 int main (int argc, char* argv[]) {
 
-	// Checking for valid program input
+	// Check for valid program input
 	// Must have 1 command line argument (filename)
 	if (argc != 2) {
 		printf("Invalid Input Format");
 		exit(1); // unsuccessful exit
 	}
 
+	// Store and open file
+	char* file_name = argv[1];
+	FILE* file_ptr = fopen(file_name, "r");
+
+	// Check if file exists
+	if (file_ptr == NULL) {
+		printf("Invalid Input Format");
+		exit(1); // unsuccessful exit
+	}
 
 }
 
